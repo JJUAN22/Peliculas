c9acdb1e76465ce5753bc875768555abff6c3789
JJUAN22 <juanhv86@hotmail.com>
2015-11-18 21:26:16 -0600

    Clases
    Actualizacion de la solucion y creacion de clases

---
+++ entPeliculas/entClasificacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockBuster.Peliculas.Business.Entity
+{
+    public class entClasificacion
+    {
+        public int id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ entPeliculas/entGenero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlockBuster.Peliculas.Business.Entity
+{
+    public class entGenero
+    {
+        public int id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
