67f24a68c30e543e7a0bf144b7c6a0a85b80d634
ivy2308 <ivy.jary@gmail.com>
2015-11-26 19:28:04 -0600

    Sin error 2
    Ok

--- Peliculas/Default.aspx.cs
+++ Peliculas/Default.aspx.cs
@@ -19,6 +19,7 @@ public partial class _Default : System.Web.UI.Page
             if (!IsPostBack)
             {
                 cargarcarrucel();
+                cargarcarrucel2();
 
             }
         }
@@ -28,78 +29,7 @@ public partial class _Default : System.Web.UI.Page
             Title = ex.Message;
         }
     }
-
-    private void CargarCartelera(string query)
-    {
-        List<entPelicula> lst = new busPelicula().Obtener();
-
-        llenarlt(lst);
-
-
-    }
-
-    private void llenarlt(List<entPelicula> lst)
-    {
-        LiteralControl literal = new LiteralControl();
-        literal.Text = "";
-        foreach (entPelicula ent in lst)
-        {
-
-
-
-            literal.Text += "         <div class=\"col-sm-4\">";
-            literal.Text += "                <div class=\"panel panel-default\">";
-            literal.Text += "  <div class=\"panel-heading\" style=\"height: 200px;overflow: hidden;\">";
-            literal.Text += "          <div style=\"height: 150px; overflow: hidden; text-align: center;\">";
-            literal.Text += "                        <img src=\"" + ent.FotoPortada + "\" class=\"img-responsive img-rounded\"alt=\"\" style=\"margin: auto;\" />";
-            literal.Text += "  </div>";
-            literal.Text += " <div>";
-            literal.Text += ent.Nombre;
-            literal.Text += "                    </div>";
-            literal.Text += "                    </div>";
-            literal.Text += "                    <div class=\"panel-body\">";
-            literal.Text += "                        <div class=\"row\" style=\"height: 150px;\">";
-            literal.Text += "                            <div class=\"col-xs-4\">";
-            literal.Text += "                                <img src=\"" + ent.FotoMini + "\"";
-            literal.Text += "                                    class=\"img-responsive img-rounded\" />";
-            literal.Text += "                            </div>";
-            literal.Text += "                            <div class=\"col-xs-8\">";
-            literal.Text += "                                <span>Año:&nbsp;&nbsp;</span><label>" + ent.AnioLanzamiento + "</label><br />";
-            literal.Text += "                                <span>Clasificacion:&nbsp;&nbsp;</span><label>" + ent.Clasificacion + "</label><br />";
-            literal.Text += "                                <span>Género:&nbsp;&nbsp;</span><label>" + ent.Genero + "</label><br />";
-            literal.Text += "                                <span>";
-            if (ent.Estatus)
-                literal.Text += "                                    <input type=\"checkbox\" value=\"estatus\" checked disabled />";
-            else
-                literal.Text += "                                <input type=\"checkbox\" disabled value=\"estatus\"/>";
-            literal.Text += "                                </span>";
-
-            literal.Text += "<a href=\"AltaEdicion.aspx?ID=" + ent.Id.ToString() + "\"><img src=\"img\\edit.png\" width=\"40px\" /</a>";
-            //    literal.Text += "    </div>";
-            literal.Text += " </div>";
-
-            // bien     literal.Text += "                            </div>";
-            literal.Text += "                        </div>";
-            literal.Text += "                    </div>";
-            //      literal.Text += "                    <div id=\"Div2\" style=\"text-align: justify; margin: 5px auto;\">";
-            literal.Text += "                        <textarea id=\"txtSinopsis\" class=\"form-control\" rows=\"5\" style=\"text-align: justify; background-color:White \" disabled=\"disabled\">" + ent.Sinopsis + "</textarea>";
-            // bien  literal.Text += "                    </div>";
-            literal.Text += "                    <div id=\"Div3\"; style=\"text-align: center;\">";
-            literal.Text += "                        <iframe src=\"" + ent.Trailer + "\" frameborder=\"0\" allowfullscreen>";
-            literal.Text += "                        </iframe>";
-            literal.Text += "                    </div>";
-            literal.Text += "                    <!--bien-->";
-            literal.Text += "                    ";
-            literal.Text += "                </div>";
-            literal.Text += "            </div>";
-        }
-
-        phCargarcartelera.Controls.Add(literal);
-
-
-
-
-    }
+    
     public void cargarcarrucel()
     {
         List<entPelicula> lst = new List<entPelicula>();
